42e2d2b2f903e22416e7f8224973cc8bad215c08
Kurt <kurt@neumediasolutions.com>
2016-11-12 11:04:36 -0500

    Added lyrics

--- lyrics.js
+++ lyrics.js
@@ -1,3 +1,3 @@
-var lyrics = ['Let it be.', 'Say it aint so.', 'Take me or leave me.', 'Its easy as life.', 'you aint ready for this jelly'];
+var lyrics = ['Let it be.', 'Say it aint so.', 'Take me or leave me.', 'Its easy as life.', 'You aint ready for this jelly'];
 
 module.exports = lyrics;
