e417117de3799cf3207c9a18484566f24d7e32be
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-12 11:23:17 -0500

    fixed conflict

--- lyrics.js
+++ lyrics.js
@@ -2,12 +2,7 @@ var lyrics = ['Let it be.', 'Say it aint so.', 'Take me or leave me.', 'Its easy
 
 module.exports = lyrics;
 
-<<<<<<< HEAD
+
 //hellooooo
-=======
 //whatsup homies -Gao
-<<<<<<< HEAD
->>>>>>> bd1f2dd97ee87e702650b7a10bb3f30dd35fb2a5
-=======
 //this world is ending
->>>>>>> 6b2fb747982c625716a5de459505a059ea1e91d3
